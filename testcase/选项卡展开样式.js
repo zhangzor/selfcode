4a065c38f462f2e097a795fbe6db049c88dd57bd
zhang <wayne_bin@qq.com>
2021-03-02 09:46:24 +0800

    竖向选项卡

---
+++ testcase/选项卡展开样式.js
@@ -0,0 +1,14 @@
+var items = document.getElementsByClassName('item');
+console.log(45, items.length);
+var lis = document.getElementsByTagName('ul');
+console.log(47, lis.length);
+for (let i = 0; i < items.length; i++) {
+    items[i].index = i;
+    items[i].onclick = function () {
+        for (let j = 0; j < lis.length; j++) {
+            lis[j].style.display = 'none';
+        }
+        lis[this.index].style.display = 'block';
+        console.log(53, this.index);
+    }
+}
